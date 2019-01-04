075c2812fbea8571d9b669e1a1d469f3648b5fc5
Benoit Lavigne <blavigne@slack-corp.com>
2019-01-04 18:02:20 +1100

    adding step2 instructions and code

--- event_step2.py
+++ event_step2.py
@@ -46,19 +46,43 @@ def event_processor(event_payload):
     if event_type == "message":
 
         # the event will have a subtype if it's an automated or bot message
-        # ignoring those and exiting early, avoiding infinite loops
+        # ignoring those and exiting early to avoid infinite loops
         if "subtype" in event_payload["event"]:
             return True
         # other cases are messages posted by users, responding
         else:
             channel_id = event_payload["event"]["channel"]
-            text = "Hi there! I've received your message :tada:.\n Congratulations, you've finished the first branch of this workshop"
+            text = "Hi there! I'm a bot who can help users understand what channels are about.\nI won't be of much use here, but if you invite me to a channel I'll welcome users when they join it!"
             response = sc.api_call("chat.postMessage",
                                    channel=channel_id, text=text)
             # Slack API will return an "ok":True or "ok":False
             success = response.get("ok")
             return success
 
-    # Non "message" events are not expected, erroring
+    elif event_type == "member_joined_channel":
+
+        channel_id = event_payload["event"]["channel"]
+        user_id = event_payload["event"]["user"]
+
+        # Using the web API to get more info about the channel
+        convo_info = sc.api_call("conversations.info", channel=channel_id)
+
+        # checking that the call was sucessful and storring the data we're interested in
+        if convo_info.get("ok") == True:
+            topic = convo_info["channel"]["topic"]["value"]
+            purpose = convo_info["channel"]["purpose"]["value"]
+        else:
+            topic, purpose = "error", "error"
+
+        # building the message we want to send the user
+        text = f"welcome to <#{channel_id}>! Here's the channel's topic: \n {topic} \n and the purpose: \n {purpose}"
+        # sending an ephemeral message to the user who just joined the channel.
+        response = sc.api_call("chat.postEphemeral",
+                               channel=channel_id, text=text, user=user_id)
+        # Slack API will return an "ok":True or "ok":False
+        success = response.get("ok")
+        return success
+
+    # event of an unexpected type, erroring
     else:
         return False
